e5baa34c69da872ff88934d59579fbd004b91bb0
Isaiah Desta <areandd@protonmail.com>
2022-08-02 15:06:41 +0000

    my age in dog years calculator.

---
+++ app.js
@@ -0,0 +1,18 @@
+let myAge = 21;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Isaiah'.toLowerCase();
+
+// console.log(earlyYears);
+// console.log(myAge);
+// console.log(laterYears);
+// console.log(myName);
+// console.log(myAgeInDogYears);
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
